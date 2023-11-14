22698ec9a8fefa0ab48e903a560259d0e5fb6760
Julian Payne <julianpayne@fr.ibm.com>
2023-11-14 16:13:22 +0100

    feat: adding a pre-commit to check dates in files

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # md-date-pre-commit
+
+This is used like this:
+
+```yaml
+repos:
+  - repo: https://github.com/juliangp/md-date-pre-commit.git
+    rev: v1.0.0
+    hooks:
+      - id: check-last-updated
+        stages: [ commit ]
+        args: [ "egrep machine-learning.md|machine-learning-cp.md" ]
+```
---
+++ check-lastupdated.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# turn on echo
+# set -x
+
+# matching_files="egrep machine-learning.md|machine-learning-cp.md"
+matching_files="$1"
+if [[ "${matching_files}" == "" ]]; then
+  matching_files="echo"
+fi
+
+today=$(date +"%Y-%m-%d")
+files=$(git diff --name-only | "${matching_files}")
+
+function validate_lastupdated() {
+  cat $1 | grep lastupdated | awk '{print $2; exit}' | tr -d '"'
+}
+
+for f in ${files}; do
+  last_updated=$(validate_lastupdated $f)
+  echo "Found last updated ${last_updated} in $f"
+  if [[ "${last_updated}" != "${today}" ]]; then
+    echo "***ERROR*** found wrong date in ${f}: expecting ${today} but found ${last_updated}"
+    exit 1
+  fi
+done
+
+exit 0
